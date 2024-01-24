cc73848181d5493b39f414577b42afb3b69ac57d
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-24 23:18:07 +0100

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+async function isFileExist(path) {
+  try {
+    return (await fsp.stat(path)).isFile();
+  } catch (e) {
+    return false;
+  }
+}
+
 const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const filePath = path.resolve(__dirname, 'files', fileName);
+  const fileContent = 'I am fresh and young';
+
+  try {
+    if (await isFileExist(filePath)) {
+      throw new Error('FS operation failed');
+    }
+
+    await fsp.writeFile(filePath, fileContent);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await create();
