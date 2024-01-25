29b8427164d3b2f81ec98430e557aee9acde5c57
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-26 00:54:28 +0100

    refactor: import dirname

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,17 +2,14 @@ import fs from 'node:fs';
 import fsp from 'node:fs/promises';
 import path from 'node:path';
 
-const __dirname = new URL('.', import.meta.url).pathname;
-
 const create = async () => {
+  const __dirname = import.meta.dirname;
   const fileName = 'fresh.txt';
   const filePath = path.resolve(__dirname, 'files', fileName);
   const fileContent = 'I am fresh and young';
 
-  const isFileExists = fs.existsSync(filePath);
-
   try {
-    if (isFileExists) {
+    if (fs.existsSync(filePath)) {
       throw new Error('FS operation failed');
     }
 
