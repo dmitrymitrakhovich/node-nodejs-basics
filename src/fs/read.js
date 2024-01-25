00c46f23403588fa0c39bc108f6fc1c2573efd03
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-26 00:45:40 +0100

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+import fsp from 'node:fs/promises';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const filePath = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    if (!fs.existsSync(filePath)) {
+      throw new Error('FS operation failed');
+    }
+
+    const content = await fsp.readFile(filePath, { encoding: 'utf-8' });
+    console.log(content);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await read();
