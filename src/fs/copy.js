989ce29798acfe646281cd0d83acd0a5b6b106e1
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-26 00:03:31 +0100

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import fs from 'node:fs';
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const checkExistPath = (path) => fs.existsSync(path);
+
 const copy = async () => {
-    // Write your code here 
+  const sourcePath = path.resolve(__dirname, 'files');
+  const targetPath = path.resolve(__dirname, 'files_copy');
+
+  try {
+    if (!checkExistPath(sourcePath) || checkExistPath(targetPath)) {
+      throw new Error('FS operation failed');
+    }
+
+    await fsp.mkdir(targetPath);
+
+    const files = await fsp.readdir(sourcePath);
+
+    for (const file of files) {
+      await fsp.copyFile(
+        path.resolve(sourcePath, file),
+        path.resolve(targetPath, file)
+      );
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await copy();
