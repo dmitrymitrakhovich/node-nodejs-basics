120597fad3153a5671a3ad32239d093fde38f06a
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-26 00:14:15 +0100

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs';
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const checkExistPath = (path) => fs.existsSync(path);
+
 const rename = async () => {
-    // Write your code here 
+  const sourcePath = path.resolve(__dirname, 'files', 'wrongFilename.txt');
+  const targetPath = path.resolve(__dirname, 'files', 'properFilename.md');
+
+  try {
+    if (!checkExistPath(sourcePath) || checkExistPath(targetPath)) {
+      throw new Error('FS operation failed');
+    }
+
+    await fsp.rename(sourcePath, targetPath);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await rename();
