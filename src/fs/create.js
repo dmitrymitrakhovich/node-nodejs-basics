b3926cb4a4c408c5f524f1b786d7899c6d6097b3
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-25 21:23:09 +0100

    refactor: use node:fs instead node:fs/promises

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,30 +1,24 @@
-import fsp from 'node:fs/promises';
+import fs from 'node:fs';
 import path from 'node:path';
 
 const __dirname = new URL('.', import.meta.url).pathname;
-
-async function isFileExist(path) {
-  try {
-    return (await fsp.stat(path)).isFile();
-  } catch (e) {
-    return false;
-  }
-}
 
 const create = async () => {
   const fileName = 'fresh.txt';
   const filePath = path.resolve(__dirname, 'files', fileName);
   const fileContent = 'I am fresh and young';
 
-  try {
-    if (await isFileExist(filePath)) {
-      throw new Error('FS operation failed');
-    }
+  const isFileExists = fs.existsSync(filePath);
 
-    await fsp.writeFile(filePath, fileContent);
-  } catch (error) {
-    throw new Error(error);
+  if (isFileExists) {
+    throw new Error('FS operation failed');
   }
+
+  fs.writeFile(filePath, fileContent, (error) => {
+    if (error) {
+      throw new Error(error);
+    }
+  });
 };
 
 await create();
