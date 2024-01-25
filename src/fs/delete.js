dede9021432c25b0f026eccf4809bbd0387739d3
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-26 00:57:13 +0100

    refactor: correct import dirname in remove fn

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -2,15 +2,12 @@ import fs from 'node:fs';
 import fsp from 'node:fs/promises';
 import path from 'node:path';
 
-const __dirname = new URL('.', import.meta.url).pathname;
-
-const checkExistPath = (path) => fs.existsSync(path);
-
 const remove = async () => {
+  const __dirname = import.meta.dirname;
   const filePath = path.resolve(__dirname, 'files', 'fileToRemove.txt');
 
   try {
-    if (!checkExistPath(filePath)) {
+    if (!fs.existsSync(filePath)) {
       throw new Error('FS operation failed');
     }
 
