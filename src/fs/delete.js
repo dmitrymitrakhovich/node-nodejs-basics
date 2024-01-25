371e53730a00189c973023d5e9af852d75a14820
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-26 00:23:10 +0100

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const checkExistPath = (path) => fs.existsSync(path);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    if (!checkExistPath(filePath)) {
+      throw new Error('FS operation failed');
+    }
+
+    await fsp.rm(filePath);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await remove();
