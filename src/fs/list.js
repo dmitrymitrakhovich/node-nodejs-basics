b03d7327a103ed26915b114b78960c3cc757e215
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-26 00:37:57 +0100

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs';
+import fsp from 'node:fs/promises';
+import path from 'node:path';
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const dirPath = path.resolve(__dirname, 'files');
+
+  try {
+    if (!fs.existsSync(dirPath)) {
+      throw new Error('FS operation failed');
+    }
+
+    const files = await fsp.readdir(dirPath);
+    const names = [];
+
+    for (const file of files) {
+      names.push(file);
+    }
+
+    console.log(files);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await list();
