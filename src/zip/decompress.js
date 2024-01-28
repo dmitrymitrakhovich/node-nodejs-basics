f8240423ed2ea534dc2885329068b9b6b6739c55
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-28 21:39:14 +0100

    feat: implement zip/decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { createUnzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const sourcePath = path.resolve(__dirname, 'files', 'archive.gz');
+  const targetPath = path.resolve(__dirname, 'files', 'fileToCompress.txt');
+
+  const unzip = createUnzip();
+
+  try {
+    const rs = fs.createReadStream(sourcePath);
+    const ws = fs.createWriteStream(targetPath);
+    await pipeline(rs, unzip, ws);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await decompress();
