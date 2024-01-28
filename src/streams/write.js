c0dcf2eba2f2ea70de6a40ee5a96e1b1bcffd497
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-28 19:47:36 +0100

    feat: implement streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const filePath = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+
+  try {
+    const ws = fs.createWriteStream(filePath);
+    await pipeline(process.stdout, ws);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await write();
