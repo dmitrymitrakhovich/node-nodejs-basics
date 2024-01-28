7b6a954dcc1fc29fec4b646a866be423e1ea525f
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-28 21:38:52 +0100

    feat: implement zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const sourcePath = path.resolve(__dirname, 'files', 'fileToCompress.txt');
+  const targetPath = path.resolve(__dirname, 'files', 'archive.gz');
+
+  const gzip = createGzip();
+
+  try {
+    const rs = fs.createReadStream(sourcePath);
+    const ws = fs.createWriteStream(targetPath);
+    await pipeline(rs, gzip, ws);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await compress();
