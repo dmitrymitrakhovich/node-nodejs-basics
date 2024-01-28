0f287784614bece0615d561c37b6ed7e9a45d0bc
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-28 23:45:46 +0100

    feat: implement wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import os from 'node:os';
+import path from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { Worker, isMainThread } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = import.meta.dirname;
+  const workerPath = path.resolve(__dirname, 'worker.js');
+
+  if (isMainThread) {
+    const cpus = os.cpus().length;
+    const result = [];
+
+    let count = 0;
+
+    for (let i = 0; i < cpus; i++) {
+      const worker = new Worker(workerPath, { workerData: 10 + i });
+
+      worker.on('message', (data) => {
+        result[i] = { status: 'success', data };
+      });
+
+      worker.on('error', (error) => {
+        result[i] = { status: 'error', data: null };
+      });
+
+      worker.on('exit', () => {
+        count += 1;
+        if (count === cpus) {
+          console.log(result);
+        }
+      });
+    }
+  }
 };
 
 await performCalculations();
