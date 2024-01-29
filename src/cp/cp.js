b7b8b13b0ca0bc919b39ba802d2bafbae248f405
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-29 22:06:58 +0100

    refactor: cp/use spawn  instead fork

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,12 +1,18 @@
-import { fork } from 'node:child_process';
+import { spawn } from 'node:child_process';
 import path from 'node:path';
+import { stderr, stdin, stdout } from 'node:process';
 
 const spawnChildProcess = async (args) => {
   const __dirname = import.meta.dirname;
   const filePath = path.resolve(__dirname, 'files', 'script.js');
 
-  fork(filePath, args);
+  const childProcess = spawn('node', [filePath, ...args], {
+    stdio: [stdin, stdout, stderr, 'ipc'],
+  });
+
+  childProcess.on('error', (error) => {
+    console.error(error);
+  });
 };
 
-// Put your arguments in function call to test this functionality
 spawnChildProcess(['hello', 'it', 'is', 'test']);
