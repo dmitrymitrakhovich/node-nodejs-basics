bb270e1a61be497924094fa990a8405dc5678b23
Dmitry Mitrakhovich <dmitrymitrakhovich@Dmitrys-MacBook-Pro-2.local>
2024-01-29 20:49:22 +0100

    implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from 'node:child_process';
+import path from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = import.meta.dirname;
+  const filePath = path.resolve(__dirname, 'files', 'script.js');
+
+  fork(filePath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hello', 'it', 'is', 'test']);
